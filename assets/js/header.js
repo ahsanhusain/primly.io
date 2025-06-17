40b1b93f800daade4d9a3b2ee9a58e7498e955f8
Talha Hori <talha.solutionq786gmail.com>
2025-06-18 02:18:28 +0500

    Icons Changes

--- assets/js/header.js
+++ assets/js/header.js
@@ -3,6 +3,14 @@ document.getElementById('hamburger').addEventListener('click', () => {
 });
 
 const userDropdown = document.getElementById('userDropdown');
-userDropdown.addEventListener('click', () => {
+userDropdown.addEventListener('click', (event) => {
     userDropdown.classList.toggle('open');
+    event.stopPropagation();
+});
+
+// Close dropdown if clicked outside
+document.addEventListener('click', (event) => {
+    if (!userDropdown.contains(event.target)) {
+        userDropdown.classList.remove('open');
+    }
 });
